7c2fd75a07ffeed0227e2860bf2e7d141a350153
ralf.lima <ralf.lima@SALA400.proway.treina>
2018-05-18 19:43:56 -0300

    Enviando arquivos.

--- src/ex09_JPassword/Principal.java
+++ src/ex09_JPassword/Principal.java
@@ -1,20 +1,34 @@
 package ex09_JPassword;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
 
 public class Principal {
 
 	public static void main(String[] args) {
 
 		// JFrame
-		JFrame formulario = new JFrame("Usando ButtonGroup");
-		formulario.setSize(500, 300);
+		JFrame formulario = new JFrame("JPassword");
+		formulario.setSize(400, 80);
 		formulario.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		formulario.setLocationRelativeTo(null);
 		formulario.setLayout(null);
 		
-		// JTextField
+		// JLabel
+		JLabel rotulo = new JLabel("Informe sua senha");
+		rotulo.setBounds(10, 10, 120, 20);
 		
+		// JTextField
+		JPasswordField senha = new JPasswordField();
+		senha.setBounds(130, 10, 240, 20);
+		
+		// Adicionar ao formulário os componentes
+		formulario.add(rotulo);
+		formulario.add(senha);
+		
+		//Exibir elementos
+		formulario.setVisible(true);
 
 	}
 
