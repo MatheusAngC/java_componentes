ef38286b6a738750ff592ee7d7393d232913896b
ralflima <ralf.lima@gmail.com>
2018-05-18 16:39:43 -0300

    Add files via upload

---
+++ src/ex09_JPassword/Principal.java
@@ -0,0 +1,21 @@
+package ex09_JPassword;
+
+import javax.swing.JFrame;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		// JFrame
+		JFrame formulario = new JFrame("Usando ButtonGroup");
+		formulario.setSize(500, 300);
+		formulario.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		formulario.setLocationRelativeTo(null);
+		formulario.setLayout(null);
+		
+		// JTextField
+		
+
+	}
+
+}
