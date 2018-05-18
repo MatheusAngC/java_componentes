588b6c69f9cb910ee4bc46d62f28d4aada5b0082
ralf.lima <ralf.lima@SALA100.proway.treina>
2018-05-18 14:49:26 -0300

    a

--- src/ex09_JPassword/Principal.java
+++ src/ex09_JPassword/Principal.java
@@ -1,9 +1,17 @@
 package ex09_JPassword;
 
+import javax.swing.JFrame;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		// JFrame
+		JFrame formulario = new JFrame("Usando ButtonGroup");
+		formulario.setSize(500, 300);
+		formulario.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		formulario.setLocationRelativeTo(null);
+		formulario.setLayout(null);
 
 	}
 
